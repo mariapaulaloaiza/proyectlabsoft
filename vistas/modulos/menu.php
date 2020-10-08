5bc831051bbf25674e13a0481d10dde33b9a6134
mariapaulaloaiza <58571961+mariapaulaloaiza@users.noreply.github.com>
2020-10-07 19:52:02 -0500

    Primera entrega

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,39 @@
+<aside class="main-sidebar">
+
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+			<li class="active">
+
+				<a href="inicio">
+
+					<i class="fa fa-home"></i>
+					<span>Inicio</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="usuarios">
+
+					<i class="fa fa-user"></i>
+					<span>Usuarios</span>
+
+				</a>
+
+			</li>
+
+			
+
+				</ul>
+
+			</li>
+
+		</ul>
+
+	 </section>
+
+</aside>

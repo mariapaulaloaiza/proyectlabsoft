a7e29410412c50c463e44d84f2184ef98f7f7247
mariapaulaloaiza <58571961+mariapaulaloaiza@users.noreply.github.com>
2020-11-05 16:39:29 -0500

    Add files via upload

---
+++ vistas/modulos/ligas.php
@@ -0,0 +1,563 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Administrar ligas
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar ligas</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+
+      <div class="box-header with-border">
+  
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarLiga">
+          
+          Agregar liga
+
+        </button>
+
+      </div>
+
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:5px"></th>
+           <th>Nombre</th>
+           <th>Deporte</th>
+           <th>Direccion</th>
+           <th>Telefono</th>
+           <th>Categoria 1</th>
+           <th>Categoria 2</th>
+           <th>Categoria 3</th>
+           <th>Acciones</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+
+        <?php
+
+        $item = null;
+        $valor = null;
+
+        $ligas = ControladorLigas::ctrMostrarLigas($item, $valor);
+
+       foreach ($ligas as $key => $value){
+         
+          echo ' <tr>
+                  <td></td>
+                  <td>'.$value["nombreliga"].'</td>
+                  <td>'.$value["deporte"].'</td>';
+
+
+                  echo '<td>'.$value["direccion"].'</td>
+                  <td>'.$value["telefono"].'</td>
+                  <td>'.$value["categoria1"].'</td>
+                  <td>'.$value["categoria2"].'</td>
+                  <td>'.$value["categoria3"].'</td>';
+
+
+                  
+                  
+                  echo '
+                  <td>
+
+                    <div class="btn-group">
+                        
+                      <button class="btn btn-warning btnEditarLiga" idLiga="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarLiga"><i class="fa fa-pencil"></i></button>
+
+                      <button class="btn btn-danger btnEliminarLiga" idLiga="'.$value["id"].'"><i class="fa fa-times"></i></button>
+
+                    </div>  
+
+                  </td>
+
+                </tr>';
+        }
+
+
+        ?> 
+
+        </tbody>
+
+       </table>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+
+<!--=====================================
+MODAL AGREGAR LIGA
+======================================-->
+
+<div id="modalAgregarLiga" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar liga</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL DEPORTE -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoDeporte" placeholder="Ingresar deporte" id="nuevoDeporte" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA DIRECCION -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevaDireccion" placeholder="Ingresar direccion" id="nuevaDireccion" required>
+
+              </div>
+
+            </div>
+
+
+          <!-- ENTRADA PARA EL TELEFONO -->
+
+              <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoTelefono" placeholder="Ingresar telefono" id="nuevoTelefono" data-inputmask="'mask':'(999) 999-9999'" data-mask required>
+
+              </div>
+
+            </div>
+
+          <!-- ENTRADA PARA EL CATEGORIA1 -->
+
+          <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <select class="form-control input-lg" id="nuevaCategoria1" name="nuevaCategoria1" required>
+                  
+                  <option value="">Selecionar categoria</option>
+
+                  <?php
+
+                  $item = null;
+                  $valor = null;
+
+                  $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                  foreach ($categorias as $key => $value) {
+                    
+                    echo '<option value="'.$value["categoria"].'">'.$value["categoria"].'</option>';
+                  }
+
+                  ?>
+  
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL CATEGORIA2 -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <select class="form-control input-lg" id="nuevaCategoria2" name="nuevaCategoria2" required>
+                  
+                  <option value="">Selecionar categoria</option>
+
+                  <?php
+
+                  $item = null;
+                  $valor = null;
+
+                  $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                  foreach ($categorias as $key => $value) {
+                    
+                    echo '<option value="'.$value["categoria"].'">'.$value["categoria"].'</option>';
+                  }
+
+                  ?>
+  
+                </select>
+
+              </div>
+
+            </div>
+
+
+            <!-- ENTRADA PARA EL CATEGORIA3 -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <select class="form-control input-lg" id="nuevaCategoria3" name="nuevaCategoria3" required>
+                  
+                  <option value="">Selecionar categoria</option>
+
+                  <?php
+
+                  $item = null;
+                  $valor = null;
+
+                  $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                  foreach ($categorias as $key => $value) {
+                    
+                    echo '<option value="'.$value["categoria"].'">'.$value["categoria"].'</option>';
+                  }
+
+                  ?>
+  
+                </select>
+
+              </div>
+
+            </div>
+  
+
+            
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar liga</button>
+
+        </div>
+
+        <?php
+
+          $crearLiga = new ControladorLigas();
+          $crearLiga -> ctrCrearLiga();
+
+        ?>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+<!--=====================================
+MODAL EDITAR LIGA
+======================================-->
+
+<div id="modalEditarLiga" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar Liga</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarNombre" name="editarNombre" value="" required>
+
+                <input type="hidden"  name="idLiga" id="idLiga" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL DEPORTE -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarDeporte" name="editarDeporte" value="" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA DIRECCION-->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarDireccion" name="editarDireccion" value="" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL TELEFONO -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarTelefono" name="editarTelefono" value="" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL CATEGORIA1 -->
+
+          <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <select class="form-control input-lg" id="editarCategoria1" name="editarCategoria1" required>
+                  
+                  <option value="">Selecionar categoria</option>
+
+                  <?php
+
+                  $item = null;
+                  $valor = null;
+
+                  $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                  foreach ($categorias as $key => $value) {
+                    
+                    echo '<option value="'.$value["categoria"].'">'.$value["categoria"].'</option>';
+                  }
+
+                  ?>
+  
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL CATEGORIA2 -->
+
+          <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <select class="form-control input-lg" id="editarCategoria2" name="editarCategoria2" required>
+                  
+                  <option value="">Selecionar categoria</option>
+
+                  <?php
+
+                  $item = null;
+                  $valor = null;
+
+                  $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                  foreach ($categorias as $key => $value) {
+                    
+                    echo '<option value="'.$value["categoria"].'">'.$value["categoria"].'</option>';
+                  }
+
+                  ?>
+  
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL CATEGORIA3 -->
+
+          <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <select class="form-control input-lg" id="editarCategoria3" name="editarCategoria3" required>
+                  
+                  <option value="">Selecionar categoria</option>
+
+                  <?php
+
+                  $item = null;
+                  $valor = null;
+
+                  $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                  foreach ($categorias as $key => $value) {
+                    
+                    echo '<option value="'.$value["categoria"].'">'.$value["categoria"].'</option>';
+                  }
+
+                  ?>
+  
+                </select>
+
+              </div>
+
+            </div>
+
+          
+
+        
+
+            
+
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Modificar liga</button>
+
+        </div>
+
+     <?php
+
+          $editarLiga = new ControladorLigas();
+          $editarLiga -> ctrEditarLiga();
+
+        ?> 
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php
+
+  $borrarLiga = new ControladorLigas();
+  $borrarLiga -> ctrBorrarLiga();
+
+?> 
+
+
